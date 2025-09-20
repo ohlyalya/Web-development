8db13200421374f1a5094b3403c1c2cabadec6a7
ohlala <yagodka.play@gmail.com>
2025-09-20 13:23:57 +0300

    gtrgtrg

---
+++ script.js
@@ -0,0 +1,11 @@
+const num1 = Math.floor(Math.random() * 100) + 1;
+const num2 = Math.floor(Math.random() * 100) + 1;
+
+const sum = num1 + num2;
+
+console.log(`%c${num1} + ${num2} = ${sum}`, 'color: yellow; font-weight: bold;');
+
+console.log('color: yellow;');
+console.log(`${num1}`, 'color: yellow;');
+console.log(`${num2}`, 'color: yellow;');
+console.log(`${sum}`, 'color: yellow; font-size: 16px; font-weight: bold;');
